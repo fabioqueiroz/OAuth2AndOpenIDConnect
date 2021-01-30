d26c08d4284f320a47a480044a87de1073ca0330
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2021-01-30 18:36:33 +0000

    authorization setup

--- src/ImageGallery.Client/Startup.cs
+++ src/ImageGallery.Client/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,7 +31,44 @@ namespace ImageGallery.Client
                 client.BaseAddress = new Uri("https://localhost:44366/");
                 client.DefaultRequestHeaders.Clear();
                 client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
-            });             
+            });
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+            {
+                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.Authority = "https://localhost:44318/";
+                options.ClientId = "imagegalleryclient";
+                options.ResponseType = "code";
+                options.UsePkce = false;
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                //options.Scope.Add("address");
+                //options.Scope.Add("roles");
+                //options.Scope.Add("imagegalleryapi");
+                //options.Scope.Add("subscriptionlevel");
+                //options.Scope.Add("country");
+                //options.Scope.Add("offline_access");
+                //options.ClaimActions.DeleteClaim("sid");
+                //options.ClaimActions.DeleteClaim("idp");
+                //options.ClaimActions.DeleteClaim("s_hash");
+                //options.ClaimActions.DeleteClaim("auth_time");
+                //options.ClaimActions.MapUniqueJsonKey("role", "role");
+                //options.ClaimActions.MapUniqueJsonKey("subscriptionlevel", "subscriptionlevel");
+                //options.ClaimActions.MapUniqueJsonKey("country", "country");
+                options.SaveTokens = true;
+                options.ClientSecret = "secret";
+                //options.GetClaimsFromUserInfoEndpoint = true;
+                //options.TokenValidationParameters = new TokenValidationParameters
+                //{
+                //    NameClaimType = JwtClaimTypes.GivenName,
+                //    RoleClaimType = JwtClaimTypes.Role
+                //};
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +91,9 @@ namespace ImageGallery.Client
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
--- src/LocalDemo.IDP/Config.cs
+++ src/LocalDemo.IDP/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -22,6 +23,36 @@ namespace LocalDemo.IDP
 
         public static IEnumerable<Client> Clients =>
             new Client[] 
-            { };
+            { 
+                new Client
+                {
+                    //AccessTokenType = AccessTokenType.Reference,
+                    //AccessTokenLifetime = 120,
+                    //AllowOfflineAccess = true,
+                    //UpdateAccessTokenClaimsOnRefresh = true,
+                    ClientName = "Image Gallery",
+                    ClientId = "imagegalleryclient",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequirePkce = false,
+                    RedirectUris = new List<string>()
+                    {
+                        "https://localhost:44389/signin-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        //IdentityServerConstants.StandardScopes.Address,
+                        //"roles",
+                        //"imagegalleryapi",
+                        //"country",
+                        //"subscriptionlevel"
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    }
+                }
+            };
     }
 }
